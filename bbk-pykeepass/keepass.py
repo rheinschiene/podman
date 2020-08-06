5d5096a41aeebce1037b02dcbb0f29755be8cd05
rheinschiene <github@ms86.de>
2020-08-06 15:00:01 +0200

    New ENV variables, changed from input stream to file read

--- bbk-pykeepass/keepass.py
+++ bbk-pykeepass/keepass.py
@@ -4,57 +4,49 @@ from pykeepass import PyKeePass
 import sys, getopt
 import select
 import os
+import pathlib
 
-if select.select([sys.stdin,],[],[],0.0)[0]:
-    print("Have data!")
-else:
-    print("No data")
-    sys.exit()
+groupname = os.environ['GROUPNAME']
+tempfile = os.environ['TEMPFILE']
+dbfile = os.environ['DBFILE']
+
+path = pathlib.Path(tempfile)
+if not path.is_file():
+   print("Datei fehlt: " + tempfile)
+   sys.exit()
+   
+path = pathlib.Path(dbfile)
+if not path.is_file():
+   print("Datei fehlt: " + dbfile)
+   sys.exit()
     
 # load database
-kp = PyKeePass('db.kdbx', password=os.environ['PASSWORD'])
-group = kp.find_groups(name='Internet', first=True)
+kp = PyKeePass(dbfile, password=os.environ['PASSWORD'])
+group = kp.find_groups(name=groupname, first=True)
 
-for line in sys.stdin:
-   # Dateiinput trennen nach ";;;" und in einem Array speichern
-   data = line.split(';;;')
-   # Letztes Zeichen (\n) löschen
-   data[-1] = data[-1].strip()
+count = 1
+with open (tempfile, "r") as rd:
+   # Read lines in loop
+   for line in rd:
+      # All lines (besides the last) will include  newline, so strip it
+      data = line.strip()
+      data = data.split(';;;')
+
+      if len(data) != 4:
+         print("Fehlerhafte Eingabe! Zeile: " + str(count))
+         print(data)
+         sys.exit()
+         
+      # Variablen setzen
+      title = str(data[0])
+      user = str(data[1])
+      password = str(data[2])
+      notes = str(data[3])
    
-   # Hat das Array genau 4 Einträge?
-   if len(data) != 4:
-      print("Fehlerhafte Eingabe!")
-      print(data)
-      sys.exit()
-      
-   if data[0] == "EOF":
-      break
-   
-   # Variablen setzen
-   title = str(data[0])
-   user = str(data[1])
-   password = str(data[2])
-   notes = str(data[3])
-   
-   # Eintrag in KeePassDB schreiben (temporär, nicht gespeichert)
-   kp.add_entry(group, title, user, password, None, notes)
+      # Eintrag in KeePassDB schreiben (temporär, nicht gespeichert)
+      kp.add_entry(group, title, user, password, None, notes)
+      count += 1
    
 kp.save()
    
 sys.exit()
-
-# Print total number of arguments
-#params = len(sys.argv)
-#print ('Total number of arguments:', params)
-
-#if params != 5:
-#	print(sys.argv[0], "title user password notes")
-#	sys.exit()
-#else:
-#	# Print all arguments
-#	print ('Argument List:', str(sys.argv))
-
-#title = str(sys.argv[1])
-#user = str(sys.argv[2])
-#password = str(sys.argv[3])
-#notes = str(sys.argv[4])
