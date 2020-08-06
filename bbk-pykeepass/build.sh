5fdbd3191ddda5d5c936f6608a3c18d96be3d163
rheinschiene <github@ms86.de>
2020-08-06 15:01:53 +0200

    Added build.sh

---
+++ bbk-pykeepass/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+IMAGE="pykeepass"
+CURRENT_VERSION=$(podman images localhost/${IMAGE} --format "{{.Tag}}" | grep -v "latest" | sort -nr | head -1)
+NEXT_VERSION=$(echo "$CURRENT_VERSION 0.1" | awk '{printf "%1.1f", $1 + $2}')
+
+echo "Current Version: $CURRENT_VERSION"
+
+if [ "$1" == "build" ]; then
+	echo "Next Version: $NEXT_VERSION"
+	podman build -f ${IMAGE}.podfile -t ${IMAGE}:$NEXT_VERSION
+	if [ $? -eq 0 ]; then
+		echo "Build erfolgreich!"
+	else
+		echo "Build fehlgeschlagen!"
+	fi
+fi
